f88a0f8af0b3e5392183c8a43e18c3f7f0e91bd4
AndersonPGS <andersonpgs2001@hotmail.com>
2022-04-13 21:14:46 -0300

    Home created

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,20 @@ import React from "react";
 import { Container } from "./styles";
 
 const Home: React.FC = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <img
+        src="https://avatars.githubusercontent.com/u/57535111?v=4"
+        alt="Github Profile"
+      />
+      <p>Anderson Paiva</p>
+      <div className="buttons">
+        <button>Github</button>
+        <button>Linkedin</button>
+        <button>Discord</button>
+      </div>
+    </Container>
+  );
 };
 
 export default Home;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,55 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background: rgba(0, 0, 0, 0.6);
+  height: 80vh;
+  width: 70%;
+  border-radius: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  transition: all 0.3s;
+  &:hover {
+    transform: scale(1.1);
+  }
+
+  img {
+    height: 300px;
+    border-radius: 100%;
+    background-color: rgba(0, 0, 0, 0.6);
+    transition: all 0.3s;
+
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.7);
+      transform: scale(1.1);
+    }
+  }
+
+  p {
+    margin: 20px;
+    font-size: 2rem;
+    color: white;
+    font-weight: bold;
+  }
+
+  button {
+    border: none;
+    border-radius: 15px;
+    height: 50px;
+    width: 100px;
+    margin: 10px;
+    background-color: rgba(0, 0, 0, 0.7);
+    color: white;
+    font-size: 1rem;
+    font-weight: bold;
+    transition: all 0.2s;
+
+    &:hover {
+      transform: scale(1.2);
+      background-color: rgba(0, 0, 0, 0.4);
+      border-radius: 10px;
+      cursor: pointer;
+    }
+  }
+`;
